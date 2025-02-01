eaf0ac533288f9d03c6d4f87136fd88d5cef2752
jzanine <jzanine@asu.edu>
2025-02-01 07:32:32 -0700

    added login and logout functions and their UI, along with scalable landing page and menu for future content

---
+++ ProjectTemplate/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Account
+    {
+		//this is just a container for all info related
+		//to an account.  We'll simply create public class-level
+		//variables representing each piece of information!
+		public int accountID;
+		public string username;
+		public string pass;
+		public string firstname;
+		public string lastname;
+		public Boolean admin;
+	}
+}
